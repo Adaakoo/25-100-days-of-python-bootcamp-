72781ecf1fbe5eaa9b4b122e81aaabe7140f481b
Adaakoo <adaminformatyka11@gmail.com>
2026-06-21 15:51:20 +0200

    Day 25 Project

---
+++ day-025-pandas/02-project/main.py
@@ -0,0 +1,46 @@
+import turtle
+from turtle import Turtle
+import pandas
+
+# def get_mouse_click_coor(x,y):
+#     print(x,y)
+#
+# screen.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+t = Turtle()
+turtle.shape(image)
+t.hideturtle()
+
+score = 0
+data = pandas.read_csv("50_states.csv")
+t.penup()
+guessed_states = []
+missing_states = []
+while score != 50:
+    answer_state = screen.textinput(title=f"Guess the state {score}/50 ", prompt="What's another state's name?")
+    if answer_state is None:
+        break
+    answer_state = answer_state.title()
+    if answer_state == "Exit":
+        for state in data.state.to_list():
+            if state not in guessed_states:
+                missing_states.append(state)
+        data_learn = pandas.DataFrame(missing_states)
+        data_learn.to_csv("States_to_learn.csv")
+        break
+    elif answer_state in data.state.to_list() and answer_state not in guessed_states:
+        guessed_state = data[data.state == answer_state]
+        x = guessed_state["x"].item()
+        y = guessed_state["y"].item()
+        t.goto(x,y)
+        t.pendown()
+        t.write(answer_state)
+        t.penup()
+        score += 1
+        guessed_states.append(answer_state)
+screen.exitonclick()
